6353467e20352ae21c15294898278af1d3bfa77e
Peppe <peppecal@gmail.com>
2012-02-12 09:15:34 +0100

    aggiunto script che genera pagine davvero statiche

---
+++ staticizzatore.php
@@ -0,0 +1,47 @@
+#!/usr/bin/php
+<?php
+// dove stanno i nostri simpatici sorgenti?
+$settings['sourcf']='./html/editme/'; 
+
+// dove vomito il risultato?
+$settings['desf']='./html/'; 
+
+/*
+ *
+ * Si, sono stronzo. E questo non è mediawiki, che controlla che l'html
+ * sia well-formed prima di mandarlo a se stessa. E no, non mi fido di 
+ * quello che scrivono gli utenti, ma questo è abbastanza condiviso.
+ *
+ * Quindi, i tag sono definiti uno alla volta. Anche per motivi di 
+ * performance: php è già abbastanza lento, vogliamo anche metterlo a 
+ * lavorare con espressioni regolari? Una riscrittura in perl di questa 
+ * merda sarebbe benvenuta, ma io non conosco perl.
+ *
+ * l'alternanza di $tag[] e $re[] deve essere STRETTAMENTE perfetta,
+ * altrimenti non sappiamo più cosa viene sostituito con cos'altro.
+ * cercherò qualcosa per sostituire ciò... forse.
+ * -- cal.
+ *
+ */
+
+$tag[] = '<!--#include virtual="ppbar.html"-->';
+ $re[] = file_get_contents('./html/inc/ppbar.html');
+
+$tag[] = '<!--#include virtual="sitenav.html"-->';
+ $re[] = file_get_contents('./html/inc/sitenav.html');
+
+
+$scandir=array_diff(
+	scandir($settings['sourcf']),
+	array('.', '..'));
+
+foreach($scandir as $name){
+	$pathin=pathinfo($name);
+	if($pathin['extension'] != 'html') continue;
+	
+	file_put_contents(
+		$settings['desf'].$name,
+		str_replace($tag, $re, file_get_contents($settings['sourcf'].$name))
+	);
+};
+?>
