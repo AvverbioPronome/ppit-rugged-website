c1bc7c4bafd20870a7fbd55c771a89052de98f99
Peppe <peppecal@gmail.com>
2012-03-15 05:04:20 +0100

    ora fa qualcosa.

--- liquidtry.php
+++ liquidtry.php
@@ -1,11 +1,26 @@
 #!/usr/bin/php
 <?php
-require_once('liquidquery.php');
-$lqfb = new liquidquery('http://apitest.liquidfeedback.org:25520/');
+require_once 'liquidquery.php';
+$lqfb = new liquidquery('http://apitest.liquidfeedback.org:25520/', 2);
 
-$drafts = $lqfb->getDrafts('all', true, true, 1);
+switch($argv[1]){
+	case 'approved':
+		$qs='initiative_winner=true&';
+		$qs .= 'issue_state=finished_with_winner&';
+		break;
+	case 'current':
+	default:
+};
 
-foreach($drafts as $draft){
-	echo $draft['initiative_id']."\n";
+$qs.='current_draft=true&';
+
+echo $qs."\n";
+
+//$drafts = $lqfb->getDrafts();
+//foreach($drafts as $draft){ echo $draft['initiative_id']."\n";}
+
+foreach($lqfb->getDrafts($qs) as $draft){
+	echo $draft['initiative_id'].': '.$draft['name']."\n";
+	echo "\t".str_replace("\n", "\n\t", wordwrap($draft['content'], 60, "\n"))."\n";
 }
 ?>
