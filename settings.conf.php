0d0cd0bc5154af5db7221197a16622f5ce4c33f4
Peppe <peppecal@gmail.com>
2012-03-16 02:44:38 +0100

    passato a costanti, organizzato array del formalfoo

--- settings.conf.php
+++ settings.conf.php
@@ -1,34 +1,33 @@
 <?php
 
 // basedir
-$basedir='.';
-
+define('BASEDIR', './');
 // dove sono i template (ie: wikipages, tribuna, verbale, etc)
-$templates='/templates/';
+define('TEMPLATEDIR', 'templates/')
 
 // dove sono gli include (ie: ppheader, ppfooter, etc)
-$includes='/includes/';
+define('INCLUDEDIR', 'includes/');
 
 // dove scrivere i file html finali
-$htdocs='/html/';
+define('HTDOCS', 'html/');
 
 // dove prelevare i testi
-$wikiurl = 'https://dev.partitopirata.org/projects/ppit/wiki/';
+define('WIKIURL', 'https://dev.partitopirata.org/projects/ppit/wiki/');
 
 // dove prelevare i lavori assembleari
-$lfapi = 'http://apitest.liquidfeedback.org:25520/';
+define('LFAPIURL', 'http://apitest.liquidfeedback.org:25520/');
 
 // Composizione Gazzetta
-$index = array("Il_Partito_Pirata", 28);
-$manifesto = array("Manifesto", 13);
-$statuto = array("Statuto", 45); 
-$iscrizione = array("Garanzia_di_Iscrizione_ed_Esclusione", 11);
-$intento = array("Lettera_di_Intento_Pirata", 4);
-$lara = array("Lettera_di_Assunzione_Responsabilità_Artistiche",5);
-$lart = array("Lettera_di_Assunzione_Responsabilità_Tecniche", 27);
-$larm = array("Lettera_di_Assunzione_Responsabilità_Uomini_Pubblicamente_Armati", 5);
-$modiscri = array("Modulo_Iscrizione_e_Certificazione",5);
-$modident = array("Modulo_Personale_del_Certificatore",5);
-$modquota = array("Modulo_Contabile_del_Certificatore",3);
+$settings['formalfoo'][] = array('Il_Partito_Pirata', 28);
+$settings['formalfoo'][] = array('Manifesto', 13);
+$settings['formalfoo'][] = array('Statuto', 45); 
+$settings['formalfoo'][] = array('Garanzia_di_Iscrizione_ed_Esclusione', 11);
+$settings['formalfoo'][] = array('Lettera_di_Intento_Pirata', 4);
+$settings['formalfoo'][] = array('Lettera_di_Assunzione_Responsabilità_Artistiche',5);
+$settings['formalfoo'][] = array('Lettera_di_Assunzione_Responsabilità_Tecniche', 27);
+$settings['formalfoo'][] = array('Lettera_di_Assunzione_Responsabilità_Uomini_Pubblicamente_Armati', 5);
+$settings['formalfoo'][] = array('Modulo_Iscrizione_e_Certificazione',5);
+$settings['formalfoo'][] = array('Modulo_Personale_del_Certificatore',5);
+$settings['formalfoo'][] = array('Modulo_Contabile_del_Certificatore',3);
 
 ?>
